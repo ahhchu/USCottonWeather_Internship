708bd8ff2d1bd90d5c827b0b54a5b63054df8297
Catherine Chu <catchuuu2103@gmail.com>
2023-06-06 14:43:32 -0400

    Fixed bug, further cleaned column names, ect

--- code/02 fieldData.R
+++ code/02 fieldData.R
@@ -12,7 +12,7 @@ library(dplyr)
 directory_path <- "data/fiber"
 file_names <- list.files(directory_path, pattern = "\\d{4} fibers\\.xlsx$", full.names = TRUE)
 file_names  # file names has all the xlsx stored in a list
- 
+
 
 # Wrangling ####################
 for (file in file_names) {
@@ -48,14 +48,41 @@ rename_columns <- function(data) {
   if ("re_gcode" %in% colnames(data)) {
     colnames(data)[colnames(data) == "re_gcode"] <- "regcode"
   }
+  if ("reg_code" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "reg_code"] <- "regcode"
+  }
+  if ("str_mpt" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "str_mpt"] <- "str_mot"
+  }
+  if ("sam" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "sam"] <- "sample"
+  }
+  if ("sample_number" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "sample_number"] <- "sample"
+  }
+  if ("hunterb" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "hunterb"] <- "b"
+  }
+  if ("plusb" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "plusb"] <- "b"
+  } 
+  if ("plus_b" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "plus_b"] <- "b"
+  }
+  if ("hunters" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "hunters"] <- "b"
+  }
+  if ("name" %in% colnames(data)) {
+    colnames(data)[colnames(data) == "name"] <- "var"
+  }
   return(data)
 }
 
 # Loop over the CSV files and create df
-for (i in 1:40) {
+for (i in 1:41) {
   file <- csv_names[i]
   df_name <- paste0("data", i)  # Generate the data frame name
-    
+  
   if (file.exists(file)) {
     assign(df_name, read_csv(file))
     assign(df_name, rename_columns(get(df_name)))  # Apply column renaming
@@ -85,12 +112,17 @@ data37$yr <- ifelse(data37$yr == 16, 2016, data37$yr)
 data38$yr <- ifelse(data38$yr == 17, 2017, data38$yr)
 data39$yr <- ifelse(data39$yr == 18, 2018, data39$yr)  
 data40$yr <- 2019
-data37$breeder <- as.numeric(data37$breeder)  
-data38$breeder <- as.numeric(data38$breeder)
+data41$yr <- 2020
+data38$breeder <- as.numeric(data38$breeder)  
+data39$breeder <- as.numeric(data39$breeder)
+# remove hyphen in 2020 (data41) 
+data41 <- data41 %>%
+  mutate(sample = as.numeric(gsub("-", "", sample)))
+
 
 
 # Loop over the data frames and change column types to correct data type
-for (i in 1:40) {
+for (i in 1:41) {
   df_name <- paste0("data", i)
   df <- get(df_name)
   mixed_cols <- sapply(df, function(col) any(!is.na(as.numeric(col))))
@@ -100,7 +132,7 @@ for (i in 1:40) {
 
 
 # create x21 
-for (i in 1:40) {
+for (i in 1:41) {
   df_name <- paste0("data", i)
   df <- get(df_name)
   df <- df %>%
@@ -113,11 +145,11 @@ mixed_cols <- sapply(combined_data, function(col) any(!is.na(as.numeric(col)) & 
 combined_data <- combined_data %>% mutate_if(mixed_cols, as.numeric)
 
 # Bind the data frames row-wise
-data_frames <- lapply(1:40, function(i) get(paste0("data", i))) 
+data_frames <- lapply(1:41, function(i) get(paste0("data", i))) 
 combined_data <- dplyr::bind_rows(data_frames)
 
-
-write_xlsx(combined_data,"output/combined_data.xlsx")
+write_csv(combined_data, "output/combined data.csv")
+write_xlsx(combined_data,"output/combined data.xlsx")
 
 
 
@@ -133,9 +165,9 @@ non_matching_count <- 0  # how many don't match
 
 # Compare rest of files with 1980 
 for (i in 2:length(file_names)) {
-    current_file <- read_csv(file_names[[i]])
-    current_column_names <- colnames(current_file)
-    
+  current_file <- read_csv(file_names[[i]])
+  current_column_names <- colnames(current_file)
+  
   # Check if column names match with the first file
   if (!identical(first_column_names, current_column_names)) {
     non_matching_files <- c(non_matching_files, file_names[i])
@@ -148,13 +180,13 @@ for (i in 2:length(file_names)) {
 # Print 
 if (length(non_matching_files) > 0) {
   message("The following files have differing columns compared to 1980 file:")
-    for (file in non_matching_files) {
+  for (file in non_matching_files) {
     message(paste("File:", file))
     message("Differing Columns:")
     if (length(non_matching_columns[[file]]) > 0) {
       for (column in non_matching_columns[[file]]) {
         message(paste("Original Column:", column))
-        }
+      }
     } else {
       message("None")
     }
@@ -173,7 +205,6 @@ if (non_matching_count > 0) {
 
 
 # Rename and Fix 
-
 
 
 
