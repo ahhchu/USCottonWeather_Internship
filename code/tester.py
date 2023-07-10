4f06d98be0ecf81b70e8fb3bbb4ee8b94be77582
Catherine Chu <catchuuu2103@gmail.com>
2023-07-10 14:16:30 -0400

    trying to hypertune paramesters with python

---
+++ code/tester.py
@@ -0,0 +1,10 @@
+
+# setup 
+import pandas as pd
+import numpy as np
+#from sklearn.ensemble import RandomForestClassifier
+
+# loading 
+data = pd.read_csv("data/daymet/finalWeatherandRegions.csv")
+
+
